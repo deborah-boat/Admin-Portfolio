d6f45fc1a92797a12880aeb362cafb27d8cb4c3d
Deborah <debbyyaaboat1992@gmail.com>
2023-08-02 23:04:56 +0200

    first commit

---
+++ Admin/database/login.sql
@@ -0,0 +1,3 @@
+CREATE TABLE `user` ( `id` INT NOT NULL AUTO_INCREMENT , `email` VARCHAR(255) NOT NULL , `password` VARCHAR(255) NOT NULL ,  PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+INSERT INTO `user` VALUES (1,'debby@gmail.com','12345');
---
+++ Admin/server/controllers/userController.js
@@ -0,0 +1,154 @@
+const mysql = require('mysql2');
+
+// Connection Pool
+// Database connection
+
+// Database connection
+
+let connection = mysql.createConnection({
+host: "localhost",
+user:"root",
+password: "Mrsowusu@92",
+database: "user_management"
+});
+
+// View Users
+exports.view = (req, res) => {
+  // User the connection
+  connection.query('SELECT * FROM user WHERE status = "active"', (err, rows) => {
+    // When done with the connection, release it
+    if (!err) {
+      let removedUser = req.query.removed;
+      res.render('home', { rows, removedUser });
+    } else {
+      console.log(err);
+    }
+    console.log('The data from user table: \n', rows);
+  });
+}
+
+// Find User by Search
+exports.find = (req, res) => {
+  let searchTerm = req.body.search;
+  // User the connection
+  connection.query('SELECT * FROM user WHERE first_name LIKE ? OR last_name LIKE ?', ['%' + searchTerm + '%', '%' + searchTerm + '%'], (err, rows) => {
+    if (!err) {
+      res.render('home', { rows });
+    } else {
+      console.log(err);
+    }
+    console.log('The data from user table: \n', rows);
+  });
+}
+
+exports.form = (req, res) => {
+  res.render('add-user');
+}
+
+// Add new user
+exports.create = (req, res) => {
+  const { first_name, last_name, email, phone, comments } = req.body;
+  let searchTerm = req.body.search;
+
+  // User the connection
+  connection.query('INSERT INTO user SET first_name = ?, last_name = ?, email = ?, phone = ?, comments = ?', [first_name, last_name, email, phone, comments], (err, rows) => {
+    if (!err) {
+      res.render('add-user', { alert: 'User added successfully.' });
+    } else {
+      console.log(err);
+    }
+    console.log('The data from user table: \n', rows);
+  });
+}
+
+
+// Edit user
+exports.edit = (req, res) => {
+  // User the connection
+  connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, rows) => {
+    if (!err) {
+      res.render('edit-user', { rows });
+    } else {
+      console.log(err);
+    }
+    console.log('The data from user table: \n', rows);
+  });
+}
+
+
+// Update User
+exports.update = (req, res) => {
+  const { first_name, last_name, email, phone, comments } = req.body;
+  connection.query('UPDATE user SET first_name = ?, last_name = ?, email = ?, phone = ?, comments = ? WHERE id = ?', [first_name, last_name, email, phone, comments, req.params.id], (err, rows) => {
+    if (!err) {
+      connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, rows) => {
+        // When done with the connection, release it
+        if (!err) {
+          res.render('edit-user', { rows, alert: `${first_name} has been updated.` });
+        } else {
+          console.log(err);
+        }
+        console.log('The data from user table: \n', rows);
+      });
+    } else {
+      console.log(err);
+    }
+    console.log('The data from user table: \n', rows);
+  });
+}
+
+// Delete User
+exports.delete = (req, res) => {
+
+  // Delete a record
+   connection.query('DELETE FROM user WHERE id = ?', [req.params.id], (err, rows) => {
+
+     if(!err) {
+       res.redirect('/');
+     } else {
+      console.log(err);
+     }
+     console.log('The data from user table: \n', rows);
+
+   });
+  }
+
+
+// View Users
+exports.viewall = (req, res) => {
+  connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, rows) => {
+    if (!err) {
+      res.render('view-user', { rows });
+    } else {
+      console.log(err);
+    }
+    console.log('The data from user table: \n', rows);
+  });
+
+}
+
+//Users Login
+connection.query('SELECT * FROM login WHERE email = ? AND password = ?', ['any-user-that-you-have-in-your-db', 'the-password-of-the-user'], (err, rows) => {
+  if (err) {
+    console.log(err);
+    return
+  }
+  console.log('User logged in Successfully.');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
